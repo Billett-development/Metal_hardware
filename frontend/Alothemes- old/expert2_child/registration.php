49a55fdbefff34d025cc9c8964f4fb7c4dc88b8a
Kaylem Billett <kbillettdev@hotmail.com>
2020-03-09 16:04:24 +0000

    Adding the initial file structure for the theme
    Adding all the base files for the theme structure.

---
+++ frontend/Alothemes- old/expert2_child/registration.php
@@ -0,0 +1,10 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+ 
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+ 
+  'frontend/Alothemes/expert2_child',
+ 
+  __DIR__
+);
